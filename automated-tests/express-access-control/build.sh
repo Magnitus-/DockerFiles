63495e2a59c283099ec138fcc3ef3ef92e1635b0
Eric Vallee <eric_vallee2003@yahoo.ca>
2015-10-12 18:24:49 -0400

    Added tests for express-access-control project.

---
+++ automated-tests/express-access-control/build.sh
@@ -0,0 +1,2 @@
+docker build -t magnitus/automated-tests:0.10-node-express-access-control -f node-0.10.df .;
+docker build -t magnitus/automated-tests:4.1-node-express-access-control -f node-4.1.df .;
---
+++ automated-tests/express-access-control/run.sh
@@ -0,0 +1,2 @@
+docker run -it --rm magnitus/automated-tests:0.10-node-express-access-control;
+docker run -it --rm magnitus/automated-tests:4.1-node-express-access-control;
