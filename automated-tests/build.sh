4263d74f359aac1a47d8ea737ef83bbbdd9d74d4
Eric Vallee <eric_vallee2003@yahoo.ca>
2015-11-30 01:33:56 -0500

    Corrected version typo.

--- automated-tests/build.sh
+++ automated-tests/build.sh
@@ -1,5 +1,5 @@
 docker build -t magnitus/automated-tests:node-0.10-base -f node-0.10-base.df .;
-docker build -t magnitus/automated-tests:node-4.1-base -f node-0.10-base.df .;
+docker build -t magnitus/automated-tests:node-4.1-base -f node-4.1-base.df .;
 for dir in */ ; do
     (cd "$dir"; ./build.sh);
 done
