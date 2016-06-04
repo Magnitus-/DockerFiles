56be7605047a00357b699b1dfcce31fa3978bcc3
Eric Vallee <eric_vallee@webificservices.com>
2016-06-04 19:35:06 -0400

    Improved paralellism in base image and base dev image builds

--- node-app/base-dev-image/code/npm_install.js
+++ node-app/base-dev-image/code/npm_install.js
@@ -4,25 +4,42 @@ const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
 var actOnModules = require('act-on-modules');
+Promise = require('bluebird');
+
+var childProcessExec = Promise.promisify(childProcess.exec, {'multiArgs': true});
+var installPromises = [];
 
 function npmInstall(directory)
 {
-    process.chdir(directory);
-    childProcess.execSync("npm install --production");
+    console.log('Npm install in directory: ' + directory);
+    installPromises.push(childProcessExec("npm install", {'cwd': directory}));
 }
 
-actOnModules(process.env.APP_DIR, npmInstall);
-actOnModules(process.env.SHARED_DIR, npmInstall);
-
-if(process.env.NPM_MODULES)
-{
-    var modules = process.env.NPM_MODULES.replace(' ','').split(';');
-    if(modules.length > 0)
-    {
-        process.chdir(process.env.HOME_DIR);
-        modules.forEach(function(module) {
-            childProcess.execSync("npm install "+module);
+Promise.all([actOnModules(process.env.APP_DIR, npmInstall), 
+             actOnModules(process.env.SHARED_DIR, npmInstall)]).then(() => {
+    Promise.all(installPromises).then((results) => {
+        results.forEach((result) => {
+            console.log(result[0]);
+            console.log(result[1]);
         });
-    }
-    
-}
+            
+        if(process.env.NPM_MODULES)
+        {
+            var modules = process.env.NPM_MODULES.replace(' ','').split(';');
+            if(modules.length > 0)
+            {
+                process.chdir(process.env.HOME_DIR);
+                modules.forEach(function(module) {
+                    childProcess.execSync("npm install "+module);
+                });
+            }
+            
+        }
+    }).catch((err) => {
+        console.log('npm install failed...');
+        console.log(err);
+    });
+}).catch((err) => {
+    console.log('npm install failed...');
+    console.log(err);
+});
--- node-app/base-image/code/link.js
+++ node-app/base-image/code/link.js
@@ -4,29 +4,67 @@ const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
 var actOnModules = require('act-on-modules');
+Promise = require('bluebird');
+
+var childProcessExec = Promise.promisify(childProcess.exec, {'multiArgs': true});
+var fsReadFile = Promise.promisify(fs.readFile);
+
+var globalLinkPromises = [];
 
 function linkGlobally(directory)
 {
-    process.chdir(directory);
-    childProcess.execSync("npm link");
+    console.log('Creating global link for directory: ' + directory);
+    globalLinkPromises.push(childProcessExec("npm link", {'cwd': directory}));
 }
+
+var dependencyLinkPromises = [];
 
 function resolveDependencies(directory)
 {
-    process.chdir(directory);
-    var package = JSON.parse(fs.readFileSync(path.join(directory, 'package.json'), 'utf8'));
-    if(package.localDependencies)
-    {
-        package.localDependencies.forEach(function(localDependency) {
-            childProcess.execSync("npm link "+localDependency);
-        });
-    }
+    console.log('Resolving dependencies in directory: ' + directory);
+    return fsReadFile(path.join(directory, 'package.json'), 'utf8').then((package) => {
+        var package = JSON.parse(fs.readFileSync(path.join(directory, 'package.json'), 'utf8'));
+        if(package.localDependencies)
+        {
+            var promises = [];
+            package.localDependencies.forEach(function(localDependency) {
+                promises.push(childProcessExec("npm link "+localDependency, {'cwd': directory}));
+            });
+            if(promises.length > 0)
+            {
+                return Promise.all(promises);
+            }
+        }
+    })
+
 }
 
 //Link shared modules globally
-actOnModules(process.env.SHARED_DIR, linkGlobally);
-
-//Resolve dependencies
-actOnModules(process.env.APP_DIR, resolveDependencies);
-actOnModules(process.env.SHARED_DIR, resolveDependencies);
+actOnModules(process.env.SHARED_DIR, linkGlobally).then(() => {
+    Promise.all(globalLinkPromises).then((results) => {
+        results.forEach((result) => {
+            console.log(result[0]);
+            console.log(result[1]);
+        });
+        
+        //Resolve dependencies
+        Promise.all([actOnModules(process.env.APP_DIR, resolveDependencies),
+                     actOnModules(process.env.SHARED_DIR, resolveDependencies)]).then(() => {
+            Promise.all(dependencyLinkPromises).catch((err) => {
+                console.log('npm link failed...');
+                console.log(err);
+            });
+        }).catch((err) => {
+            console.log('npm link failed...');
+            console.log(err);
+        });
+    
+    }).catch((err) => {
+        console.log('npm link failed...');
+        console.log(err);
+    });
+}).catch((err) => {
+    console.log('npm link failed...');
+    console.log(err);
+});
 
