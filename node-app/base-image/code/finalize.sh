04b778345c8b730b347c6a03eec23592ba36c383
Eric Vallee <eric_vallee@webificservices.com>
2016-06-04 16:25:28 -0400

    Refactoring on base image

--- node-app/base-image/code/finalize.sh
+++ node-app/base-image/code/finalize.sh
@@ -1,8 +1,8 @@
 echo "node-app:x:${UID}:${UID}:node-app,,,:/home/node-app:/bin/bash" >> /etc/passwd;
 echo "node-app:x:${UID}:" >> /etc/group;
 
-npm_install.js;
-link.js;
+/opt/app-setup/npm_install.js;
+/opt/app-setup/link.js;
 
 chown -R ${UID}:${UID} /home/node-app;
 chown -R ${UID}:${UID} ${SHARED_DIR};
