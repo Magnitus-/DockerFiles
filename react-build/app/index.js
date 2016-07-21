292d3eca756468fe61b14891148b8fa89ed455ca
Eric Vallee <eric_vallee@webificservices.com>
2016-07-21 03:05:45 -0400

    Progress with react compiler

--- react-build/app/index.js
+++ react-build/app/index.js
@@ -1,8 +1,35 @@
-const gulp = require('gulp');
 const browserify = require('browserify');
 const babelify = require('babelify');
 const watchify = require('watchify');
 const fs = require('fs');
 
 const settings = JSON.parse(fs.readFileSync(process.env.BUILD_FILE, 'utf8'));
-console.log(settings);
+
+var b = browserify();
+
+function recursivelyAdModules(path)
+{
+    
+}
+
+Object.keys(settings['modules']).forEach((key) => {
+    if(key != '*')
+    {
+        b.require(settings['modules'][key], {'expose': key})
+    }
+    else
+    {
+        settings['modules'][key].forEach((path) => {
+            recursivelyAdModules(path);
+        });
+    }
+});
+
+if(settings.dependencies && settings.dependencies.length)
+{
+    settings.dependencies.forEach((dependency) => {
+    });
+}
+
+b.bundle().pipe(fs.createWriteStream(settings.destination));
+
