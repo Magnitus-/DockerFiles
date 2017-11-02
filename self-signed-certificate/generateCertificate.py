6d6a4d0e07ec5245dccf5138b7366f391087b701
Eric Vallee <eric_vallee@webificservices.com>
2017-11-01 22:30:26 -0400

    Fix to ensure all 3 ways to output files work

--- self-signed-certificate/generateCertificate.py
+++ self-signed-certificate/generateCertificate.py
@@ -4,19 +4,34 @@ import subprocess
 TEMP_CONFIG_FILE = os.environ.get("TEMP_CONFIG_FILE", "/var/tmp/openssl.cnf")
 TEMP_SAN_FILE = os.environ.get("TEMP_SAN_FILE", "/var/tmp/san.cnf")
 
+fileTypes = {
+    'key_file': {'extension': '.key', 'environment_variable': 'KEY_FILE'},
+    'csr_file': {'extension': '.csr', 'environment_variable': 'CSR_FILE'},
+    'certificate_file': {'extension': '.crt', 'environment_variable': 'CERTIFICATE_FILE'}
+}
+
 def getEnv():
     env = {}
     env['domains'] = os.environ.get('DOMAINS').split(';')
-    env['key_file'] = os.path.join(os.environ.get('OUTPUT_DIR'), os.environ.get('KEY_FILE') if os.environ.get('KEY_FILE') != '' else env['domains'][0] + '.key')
-    env['csr_file'] = os.path.join(os.environ.get('OUTPUT_DIR'), os.environ.get('CSR_FILE') if os.environ.get('CSR_FILE') != '' else env['domains'][0] + '.csr')
-    env['certificate_file'] = os.path.join(os.environ.get('OUTPUT_DIR'), os.environ.get('CERTIFICATE_FILE') if os.environ.get('CERTIFICATE_FILE') != '' else env['domains'][0] + '.crt')
+
+    for fileType in ['key_file', 'csr_file', 'certificate_file']:
+        envVar = fileTypes[fileType]['environment_variable']
+        ext = fileTypes[extension]['environment_variable']
+        if os.environ.get(envVar) == None:
+            env[fileType] = 'domain' + ext
+        else:
+            env[fileType] = os.environ.get(envVar) if os.environ.get(envVar) != '' else env['domains'][0] + ext
+        env[fileType] = os.path.join(os.environ.get('OUTPUT_DIR'), env[fileType])
+
     if os.environ.get('KEY_PASSWORD') != None:
         env['password'] = 'pass:' + os.environ.get('KEY_PASSWORD')
     elif os.environ.get('KEY_PASSWORD_FILE') != None:
         env['password'] = 'file:' + os.environ.get('KEY_PASSWORD_FILE')
     else:
         env['password'] = None
+
     env['san'] = ",".join(["DNS:" + domain for domain in env['domains']])
+
     env['subj'] = "/C={COUNTRY}/ST={STATE}/L={CITY}/O={ORGANIZATION}/OU={DEPARTMENT}/CN={DOMAIN}/emailAddress={EMAIL}".format(**{
         'COUNTRY': os.environ.get('COUNTRY'),
         'STATE': os.environ.get('STATE'),
@@ -26,6 +41,7 @@ def getEnv():
         'DOMAIN': env['domains'][0],
         'EMAIL': os.environ.get('EMAIL')
     })
+
     env['key_encryption_cypher'] = os.environ.get('KEY_ENCRYPTION_CYPHER')
     env['certificate_duration'] = os.environ.get('CERTIFICATE_DURATION')
     env['key_bits'] = os.environ.get('KEY_BITS')
