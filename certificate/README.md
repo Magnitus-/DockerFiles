75c906a790eefe51d7765957c2831ac6e8cb6292
Eric Vallee <eric_vallee@webificservices.com>
2018-02-04 16:51:58 -0500

    Certificate directory renaming. Documentation adjustment for CA signed certificates. Code adjustments for CA signed certificates

--- certificate/README.md
+++ certificate/README.md
@@ -1,12 +1,10 @@
-# self-signed certificate
+# certificate
 
 ## Overview
 
-This image is used to generate self-signed certificates and is based on my understanding reading TLS certificates literature and various tutorials I found online.
+This image is used to generate certificates and is based on my understanding reading TLS certificates literature and various tutorials I found online.
 
-When developping, or sometimes for strictly internal needs (ex: internal services, build machines, etc) where you forego running your own CA in favor of a certificates whitelist, you'll want to generate 100% internal certificate(s) for TLS that you don't have to validate externally with a CA and the openssl commands to achieve this are just complicated enough to warrant being automated.
-
-This image is meant to do that with enough composability to be re-usable across projects.
+The image can be used to generate self-signed certificates or CA-signed certificates (if you manage your own internal certificate authority)
 
 ## Usage
 
@@ -14,9 +12,11 @@ Containers launched from this image will generate 3 files in an output directory
 
 - The certificate (file ending with the .crt suffix)
 - The certificate signing request (file ending with the .csr suffix)
-- The private key used to sign the certificate (file ending with the .key suffix)
+- The private key used to sign the certificate signing request and in the case of a self-signed certificate, the certificate as well (file ending with the .key suffix)
 
 The certificate will be (re)generated each time a container is ran from the image, but the private key and certificate signing requests will only be generated once if they are found in the expected directory on subsequent executions.
+
+You can pass a CA key and its accompanying certificate if you want a CA-signed certificate as opposed to a self-signed certificate. Those files won't be generated for you, but you can generate them in a prior invocation of the image by generating a self-signed certificate which can act as your CA credentials.
 
 The container's behavior can be customized with the following environment variables:
 
@@ -37,15 +37,34 @@ The container's behavior can be customized with the following environment variab
 - **KEY_ENCRYPTION_CYPHER**: Encryption cypher used to encrypt the private key file with your password. Google "openssl genrsa cyphers" for a list of valid cyphers. At the time of this writing, I see the following cyphers in the documentation: aes128, aes192, aes256, camellia128, camellia192, camellia256, des, des3, idea
 - **KEY_BITS**: Size of the generated private key in bits (defaults to 2048 if omitted)
 - **OUTPUT_CERTIFICATE_INFO**: Will output the generated certificate info if set to "true"
+- **CA_KEY_FILE**: Path to a pre-existing CA key file if you want to sign the certificate using a pre-existing CA key. It can be omitted for self-signed certificates. If present, the **KEY_FILE** will still be used to sign the certificate signing request.
+- **CA_CERTIFICATE_FILE**: Path to a pre-existing CA certificate signed with the CA key file. This environment variable should be specified if and only if the **CA_KEY_FILE** environment varialbe is also specified.
+- **CA_KEY_PASSWORD**: Password for the **CA_KEY_FILE** if it is password protected.
+- **CA_KEY_PASSWORD_FILE**: Similar to the key **CA_KEY_PASSWORD** environment variable above, except that you are specifying the file where the password is to be found instead
+- **CA_SERIALIZATION_FILE**: Path to the CA serialization file (containing a number that gets incremented each time the CA certificate/key are used during a certificate signature). If no value is set, a serialization file will automatically be generated with same path as **CA_CERTIFICATE_FILE** and the same filename ending with a **.srl** extension.
 
-## example
+## Examples
 
-See the **example** directory for a properly configured docker-compose file that you can play with. You can execute it by running ```docker-compose up``` in the example directory.
+...
+
+## tests
+
+...
 
 ## Versioning
 
-I don't foresee that much changes (changes will be driven by the evolution of my needs for self-signed certificates which aren't much) so this topic may be moot...
+I don't foresee that much changes (changes will be driven by the evolution of my needs for generating certificates which aren't much) so this topic may be moot...
 
-Stable versions are labeled ```v<NO>``` where ```NO``` is a version number and any update within the same version number will remain backward compatible.
+Versions are labeled ```X``` and ```X.Y``` and ```X.Y.Z```, where **X** is the major version number, **Y** is the minor version number and **Z** is the bug version number (following the semver convention).
 
 The **latest** tag may change in a way that breaks the previous release, so if you need something reliable, I recommend sticking with a particular version and not the **latest** tag.
+
+## Previous Image Name
+
+I previously created a repo on Docker hub for self-signed certificates called **self-signed-certificate-generator**.
+
+After I determined that I also needed certificates generated from internal certificate authority to make the most of various tools, I created a new image name more appropriately called **certificate-generator**.
+
+However, the code of **certificate-generator** is simply a more flexible adaptation of the code for **self-signed-certificate-generator**.
+
+More specifically, for self-signed certificates, **magnitus/certificate-generator:1** is backward compatible with **magnitus/self-signed-certificate-generator:v2**.
---
+++ certificate/generateCertificate.py
@@ -0,0 +1,158 @@
+import os
+import subprocess
+
+TEMP_CONFIG_FILE = os.environ.get("TEMP_CONFIG_FILE", "/var/tmp/openssl.cnf")
+TEMP_SAN_FILE = os.environ.get("TEMP_SAN_FILE", "/var/tmp/san.cnf")
+
+fileTypes = {
+    'key_file': {'extension': '.key', 'environment_variable': 'KEY_FILE'},
+    'csr_file': {'extension': '.csr', 'environment_variable': 'CSR_FILE'},
+    'certificate_file': {'extension': '.crt', 'environment_variable': 'CERTIFICATE_FILE'}
+}
+
+def getPassword(prefix):
+    if os.environ.get(prefix + '_PASSWORD') != None:
+        return 'pass:' + os.environ.get(prefix + '_PASSWORD')
+    elif os.environ.get(prefix + '_PASSWORD_FILE') != None:
+        return 'file:' + os.environ.get(prefix + '_PASSWORD_FILE')
+    else:
+        return None
+
+def getSerializationFile():
+    if os.environ.get('CA_SERIALIZATION_FILE') != None:
+        return os.environ.get('CA_SERIALIZATION_FILE')
+    elif os.environ.get('CA_CERTIFICATE_FILE') != None:
+        caCertificateFile = os.environ.get('CA_CERTIFICATE_FILE')
+        baseName = os.path.splitext(os.path.basename(caCertificateFile))[0]
+        dirName = os.path.dirname(caCertificateFile)
+        fullFilename = os.path.join(dirName, baseName + '.srl')
+        if os.path.isfile(fullFilename):
+            return fullFilename
+
+    return None
+
+def getEnv():
+    env = {}
+    env['domains'] = os.environ.get('DOMAINS').split(';')
+
+    for fileType in ['key_file', 'csr_file', 'certificate_file']:
+        envVar = fileTypes[fileType]['environment_variable']
+        ext = fileTypes[fileType]['environment_variable']
+        if os.environ.get(envVar) == None:
+            env[fileType] = 'domain' + ext
+        else:
+            env[fileType] = os.environ.get(envVar) if os.environ.get(envVar) != '' else env['domains'][0] + ext
+        env[fileType] = os.path.join(os.environ.get('OUTPUT_DIR'), env[fileType])
+
+    env['password'] = getPassword('KEY')
+
+    if os.environ.get('CA_KEY_FILE') !== None:
+        env['ca_key_file'] = os.environ.get('CA_KEY_FILE')
+        env['ca_certificate_file'] = os.environ.get('CA_CERTIFICATE_FILE')
+        env['ca_password'] = getPassword('CA_KEY')
+        env['ca_serialization_file'] = getSerializationFile()
+    else:
+        env['ca_key_file'] = None
+        env['ca_certificate_file'] = None
+        env['ca_password'] = None
+
+    env['san'] = ",".join(["DNS:" + domain for domain in env['domains']])
+
+    env['subj'] = "/C={COUNTRY}/ST={STATE}/L={CITY}/O={ORGANIZATION}/OU={DEPARTMENT}/CN={DOMAIN}/emailAddress={EMAIL}".format(**{
+        'COUNTRY': os.environ.get('COUNTRY'),
+        'STATE': os.environ.get('STATE'),
+        'CITY': os.environ.get('CITY'),
+        'ORGANIZATION': os.environ.get('ORGANIZATION'),
+        'DEPARTMENT': os.environ.get('DEPARTMENT'),
+        'DOMAIN': env['domains'][0],
+        'EMAIL': os.environ.get('EMAIL')
+    })
+
+    env['key_encryption_cypher'] = os.environ.get('KEY_ENCRYPTION_CYPHER')
+    env['certificate_duration'] = os.environ.get('CERTIFICATE_DURATION')
+    env['key_bits'] = os.environ.get('KEY_BITS')
+    env['output_certificate_info'] = os.environ.get('OUTPUT_CERTIFICATE_INFO') == 'true'
+    return env
+
+def getGenerateKeyCmd(env):
+    generateKeyCmd = ["openssl", "genrsa",
+                      "-out", env['key_file']]
+
+    if env['password'] != None:
+        generateKeyCmd += ["-" + env['key_encryption_cypher'],
+                           "-passout", env['password']]
+
+    generateKeyCmd += [env['key_bits']]
+
+    return generateKeyCmd
+
+def getGenerateCsrCmd(env):
+    generateCsrCmd = ["openssl", "req", "-new",
+                      "-key", env['key_file'],
+                      "-out", env['csr_file'],
+                      "-subj", "\"" + env['subj'] + "\"",
+                      "-extensions", "SAN",
+                      "-reqexts", "SAN",
+                      "-config", TEMP_CONFIG_FILE]
+
+    if env['password'] != None:
+        generateCsrCmd += ["-passin", env['password']]
+
+    return generateCsrCmd
+
+def getGenerateCrtCmd(env):
+    generateCrtCmd = ["openssl", "x509",
+                      "-in", env['csr_file'],
+                      "-req", "-days", env['certificate_duration'],
+                      "-out", env['certificate_file'],
+                      "-extfile", TEMP_SAN_FILE]
+
+    if env['ca_key_file'] != None:
+        generateCrtCmd += ["-CA", env['ca_certificate_file'],
+                           "-CAKey", env['ca_key_file']]
+        if env['ca_password'] != None:
+            generateCrtCmd += ["-passin", env['ca_password']]
+
+        if env['ca_serialization_file'] != None:
+            generateCrtCmd += ["-CAserial", env['ca_serialization_file']]
+        else:
+            generateCrtCmd += ["-CAcreateserial"]
+    else:
+        generateCrtCmd += ["-signkey", env['key_file']]
+        if env['password'] != None:
+            generateCrtCmd += ["-passin", env['password']]
+
+    return generateCrtCmd
+
+def getCmds(env):
+    cmds = {}
+    cmds['generate_key'] = getGenerateKeyCmd(env)
+    cmds['generate_csr'] = getGenerateCsrCmd(env)
+    cmds['generate_crt'] = getGenerateCrtCmd(env)
+
+    cmds['view_crt'] = ["openssl", "x509",
+                        "-in", env['certificate_file'],
+                        "-text", "-noout"]
+
+    return cmds
+
+if __name__ == "__main__":
+    env = getEnv()
+    cmds = getCmds(env)
+    if not os.path.isfile(env['key_file']):
+        print subprocess.check_output(" ".join(cmds['generate_key']), shell=True)
+
+    if not os.path.isfile(env['csr_file']):
+        with open("/etc/ssl/openssl.cnf", "r") as config_file:
+            with open(TEMP_CONFIG_FILE, "w") as temp_config_file:
+                config_content = config_file.read()
+                temp_config_file.write(config_content.replace("# copy_extensions = copy", "copy_extensions = copy") + "\n[SAN]\nsubjectAltName=\"" + env['san'] + "\"")
+
+        print subprocess.check_output(" ".join(cmds['generate_csr']), shell=True)
+
+    with open(TEMP_SAN_FILE, "w") as temp_san_file:
+        temp_san_file.write("subjectAltName=" + env['san'])
+    print subprocess.check_output(" ".join(cmds['generate_crt']), shell=True)
+
+    if env['output_certificate_info']:
+        print subprocess.check_output(" ".join(cmds['view_crt']), shell=True)
